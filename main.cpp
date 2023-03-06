9bb3d4146dd018eb0c082f52d6de3d422a00587a
ShivamPatel200 <79231571+ShivamPatel200@users.noreply.github.com>
2023-03-06 15:08:32 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "in_class_lab_seven.h"
+using namespace std;
+
+int main() {
+
+    vector<int> testing_ints{6, 19, 28, 250, 496, 1000, 8128};
+    execute_perfect_check(testing_ints);
+    return 0;
+}
